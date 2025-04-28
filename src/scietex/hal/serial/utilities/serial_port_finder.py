8e84c90c1d1207734a934d250dbc4fb105b300f6
bond_anton <bond.anton@gmail.com>
2025-04-28 09:58:09 +0300

    Version 1.0.3. Proper type hints for USB serial port finders.

--- src/scietex/hal/serial/utilities/serial_port_finder.py
+++ src/scietex/hal/serial/utilities/serial_port_finder.py
@@ -13,7 +13,7 @@ STM_CDC_DEVICES = {STM_VID: [STM_PID]}
 RS485_DEVICES = {0x1A86: [0x7523]}  # Sunplus Technology Inc.
 
 
-def find_serial_ports(vid_pid_mapping: dict[int, list[int]]):
+def find_serial_ports(vid_pid_mapping: dict[int, list[int]]) -> list[str]:
     """Find serial ports by VID/PID."""
     ports = serial.tools.list_ports.comports()
     selected_ports = []
@@ -24,11 +24,11 @@ def find_serial_ports(vid_pid_mapping: dict[int, list[int]]):
     return selected_ports
 
 
-def find_stm32_cdc():
+def find_stm32_cdc() -> list[str]:
     """Find STM32 CDC devices."""
     return find_serial_ports(STM_CDC_DEVICES)
 
 
-def find_rs485():
+def find_rs485() -> list[str]:
     """Find RS485 USB converters."""
     return find_serial_ports(RS485_DEVICES)
