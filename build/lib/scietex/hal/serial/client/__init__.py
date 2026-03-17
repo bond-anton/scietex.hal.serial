7c8dfcc23eae0ea7ae671d599b75f4d0956828e7
bond.anton <bond.anton@gmail.com>
2026-03-17 10:40:37 +0300

    .gitignore update

--- build/lib/scietex/hal/serial/client/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Provides RS485Client class."""
-
-from .rs485_client import RS485Client
-
-__all__ = ["RS485Client"]
--- build/lib/scietex/hal/serial/version.py
+++
@@ -1,8 +0,0 @@
-"""
-Module containing the package version information.
-
-Attributes:
-    __version__ (str): The current version of the package.
-"""
-
-__version__ = "1.3.0"
